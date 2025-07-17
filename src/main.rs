bc6472c9877862233e18e8648595821cab222353
mpvio <suryaragavan2@outlook.com>
2025-07-17 09:47:33 +0100

    prototype for version r works (no error handling)

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+use std::io;
+use rand::seq::SliceRandom;
+
+#[derive(Debug)]
 pub enum Cards {
     AceSpade,
     AceClub,
@@ -18,8 +22,76 @@ impl Cards {
 
 fn main() {
     println!("Hello, world!");
+    version_r();
 }
 
-fn win_condition(a: Cards, b: Cards) -> bool {
+fn version_r() {
+    let mut deck = prepare_deck(false);
+    let chosen = select_cards(&mut deck);
+    match (chosen.first(), chosen.last()) {
+        (Some(a), Some(b)) => {
+            println!("{}: {:#?} & {:#?}", win_condition(a, b), a, b);
+        },
+        (_, _) => {}
+    }
+    if play_again() {
+        version_r();
+    };
+}
+
+fn prepare_deck(version_y: bool) -> Vec<Cards> {
+    let mut cards = vec![Cards::AceSpade, Cards::AceClub, Cards::AceHeart, Cards::AceDiamond];
+    if version_y {
+        cards.push(Cards::Joker);
+    }
+    let mut rng = rand::rng();
+    cards.shuffle(&mut rng);
+    return cards;
+}
+
+fn select_cards(deck: &mut Vec<Cards>) -> Vec<&Cards> {
+  let mut buffer = String::new();
+  let mut selections: Vec<&Cards> = vec![];
+  println!("Enter two numbers from 1 to {}, separated by a space:", deck.len());
+  let stdin = io::stdin();
+  let result = match stdin.read_line(&mut buffer) {
+    Ok(_) => {
+        buffer.trim().to_string()
+    },
+    Err(_) => String::new(),
+  };
+  let ls = result.split_ascii_whitespace();
+  for val in ls.into_iter() {
+    match val.parse::<usize>() {
+        Ok(num) => {
+            if let Some(card) = deck.get(num-1) {
+                selections.push(card);
+            };
+        },
+        Err(_) => {
+
+        },
+    };
+  }
+  selections
+  
+}
+
+fn play_again() -> bool {
+    let mut buffer = String::new();
+    println!("Play again? Y/N:");
+    let stdin = io::stdin();
+    match stdin.read_line(&mut buffer) {
+        Ok(_) => {
+            let answer = buffer.trim().to_string();
+            answer.contains("Y") || answer.contains("y")
+        },
+        Err(_) => {
+            play_again()
+        },
+    }
+}
+
+fn win_condition(a: &Cards, b: &Cards) -> bool {
     a.color() == b.color()
 }
